04596f6e0ff90f1d846c0d7c5b37a5516b7673b9
Cameron <cameron@cameronvwilliams.com>
2015-02-15 19:44:59 -0500

    fix ids in appointmet

--- app.js
+++ app.js
@@ -46,8 +46,10 @@ var tom = new Client({first_name: 'Tom',last_name: 'Hanks',phone_num:'1800999999
  });
  */
  
- var landscape = new Appointment(notes: "Test Note",lat: 38.897676,lon: -77.03653);
- 
+ var landscape = new Appointment({client_id: new ObjectId("54e1114f0fa1f90300000003"),employee_id: new ObjectId("54e1114f0fa1f90300000001"), notes: "Test Note",lat: 38.897676,lon: -77.03653});
+ landscape.save(function (err) {
+	if (err) return handleError(err);
+ });
 // Configure express
 app.configure('development', function() {
   mongoose.connect('mongodb://localhost/appointments');
